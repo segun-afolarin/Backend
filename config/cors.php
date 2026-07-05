7daf0a2f754ec1cc93a206b57bcdd998bb0f781d
Segun Afolarin <afolarinoluwasegun811@gmail.com>
2026-07-05 02:47:33 +0100

    update

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://my-react-js-pwk1.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
